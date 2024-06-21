1c0ec9290b967d1cd7dcd324fa50b3bae263e85b
Nguyễn Trung Kiên <trungkiennguyen0310@gmail.com>
2024-06-21 12:25:53 +0700

    init project and create splash screen

---
+++ app/src/main/java/com/example/todolist/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.todolist.ui.main.SplashFragment;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, SplashFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
